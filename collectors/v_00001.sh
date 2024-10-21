b5cfc8d9ce6fa0e1eeca6ebe9e8dfb293cf009a3
BrindhaBCS <128508368+BrindhaBCS@users.noreply.github.com>
2024-10-21 13:20:45 +0530

    Delete collectors/v_00001.sh

--- collectors/v_00001.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2)
-if [[ -z "$os_version" ]]; then
-    printf 'os_version="Parameter not found"\n'
-else
-    printf 'os_version=%s\n' "$os_version"
-fi
