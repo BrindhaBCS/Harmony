125c0d0d8d76bc15af542389419725a96154e61c
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-02-04 17:35:47 +0530

    Update oracledir.sh

--- collectors/oracledir.sh
+++ collectors/oracledir.sh
@@ -19,7 +19,7 @@ else
     elif (( usage >= fs_warning_threshold )); then
         echo "$oracle_mount filesystem usage = Warning ${usage}% used"
     else
-        echo "$oracle_mount filesystem usage = OK ${usage}% used"
+        echo "$oracle_mount filesystem usage = OK"
     fi
 fi
 
