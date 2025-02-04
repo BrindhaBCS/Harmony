82018c238faa992934bb98f1c8afe75cf80ed20f
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-02-04 20:38:52 +0530

    Update timezonecheck.sh

--- collectors/timezonecheck.sh
+++ collectors/timezonecheck.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Pass the expected timezone as an argument
-EXPECTED_TIMEZONE=$1
+TIMEZONE=$1
 
 # Get the current timezone
 CURRENT_TIMEZONE=$(timedatectl | grep "Time zone" | awk '{print $3}')
 
-# Compare with the expected timezone
-if [[ "$CURRENT_TIMEZONE" == "$EXPECTED_TIMEZONE" ]]; then
+# Compare with the provided timezone
+if [[ "$CURRENT_TIMEZONE" == "$TIMEZONE" ]]; then
     echo "Time zone correct = $CURRENT_TIMEZONE"
 else
-    echo "Time zone not correct = $CURRENT_TIMEZONE (Expected: $EXPECTED_TIMEZONE)"
+    echo "Time zone not correct = $CURRENT_TIMEZONE (Expected: $TIMEZONE)"
 fi
