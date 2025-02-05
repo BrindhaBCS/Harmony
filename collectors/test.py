47427fc1396e9b74d8013d53fb7c5547a9c82e8d
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-02-05 17:41:49 +0530

    Update and rename test.py to test.sh

--- collectors/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#test
-# Simple script to display the current date and time
-from datetime import datetime
-
-current_date = datetime.now()
-print(f"The current date and time is: {current_date}")
-
-#test
---
+++ collectors/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Simple script to display the current date and time
+current_date=$(date +"%Y-%m-%d %H:%M:%S")
+echo "The current date and time is: $current_date"
