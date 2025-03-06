a229fac9a56247ab6a95f23ca12602801ac701c1
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-06 11:11:16 +0530

    Update TCP_Retries.sh

--- collectors/TCP_Retries.sh
+++ collectors/TCP_Retries.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-expected_tcp_retries2=8    #expected value =8 (for ASCS,ERS, APP),expected value =15 (for others)
+#expected_tcp_retries2=8    #expected value =8 (for ASCS,ERS, APP),expected value =15 (for others)
 param="net.ipv4.tcp_retries2"
-param_name="TCP Retries"
-
 # Get the current value
 current_value=$(sysctl -n "$param" 2>/dev/null)
 
 if [[ -z "$current_value" ]]; then
-    echo "$param = Parameter Not Found"
-    
-elif [[ "$current_value" -eq "$expected_tcp_retries2" ]]; then
-    echo "$param = $current_value"
+    echo "$param = Parameter Not Found" 
+#elif [[ "$current_value" -eq "$expected_tcp_retries2" ]]; then
+    #echo "$param = $current_value"
 else
-    echo "$param = $current_value (expected value $expected_tcp_retries2)"
+    echo "$param = $current_value"
 fi
