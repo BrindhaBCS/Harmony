143e66d9f03455795bc5eba28f92c4b8b92519b6
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-06 11:12:19 +0530

    Update TCP_Keepalive_Probes.sh

--- collectors/TCP_Keepalive_Probes.sh
+++ collectors/TCP_Keepalive_Probes.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-expected_keepalive_probes=9
+#expected_keepalive_probes=9
 param="net.ipv4.tcp_keepalive_probes"
 # Get the current value
 current_value=$(sysctl -n "$param" 2>/dev/null)
 if [[ -z "$current_value" ]]; then
     echo "$param = Parameter Not Found"
-    
-elif [[ "$current_value" -eq "$expected_keepalive_probes" ]]; then
-    echo "$param = $current_value"
+#elif [[ "$current_value" -eq "$expected_keepalive_probes" ]]; then
+    #echo "$param = $current_value"
 else
-    echo "$param = $current_value (expected value $expected_keepalive_probes)"
+    echo "$param = $current_value"
 fi
