a2fd6984d9b221da5c6b432c02d2e052e297c152
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-10 18:21:15 +0530

    Update global_name.sh

--- collectors/global_name.sh
+++ collectors/global_name.sh
@@ -3,11 +3,10 @@ SID="$SAP_SID"
 parameter="GLOBAL_NAME"
 tns_file="/sapmnt/${SID}/profile/oracle/tnsnames.ora"
 
-global_name_check=$(grep -i 'GLOBAL_NAME' $tns_file | awk -F'GLOBAL_NAME *= *' '{print $2}' | awk -F')' '{print $1}')
+command_output=$(grep -i 'GLOBAL_NAME' $tns_file | awk -F'GLOBAL_NAME *= *' '{print $2}' | awk -F')' '{print $1}')
 
-if [ -z "$global_name_check" ]; then
-    echo "$parameter" = "parameter not found"
+if [ -z "$command_output" ]; then
+    printf "%s = Not found\n" "$parameter_name"
 else
-    echo "$parameter" = "$global_name_check"
-
+    printf "%s = %s\n" "$parameter_name" "$command_output"
 fi
