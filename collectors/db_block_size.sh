a3644e404809c27f44b5480b6a91cb280b3acbda
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-10 18:20:36 +0530

    Update db_block_size.sh

--- collectors/db_block_size.sh
+++ collectors/db_block_size.sh
@@ -3,7 +3,7 @@ SID="$DB_SID"
 parameter="db_block_size"
 init_file="/oracle/${SID}/19/dbs/init.ora"
 
-block_size_check=$(grep -i "^[[:space:]]*$parameter" $init_file | awk -F'=' '{gsub(/[[:space:]]*/, "", $2); print $2}')
+command_output=$(grep -i "^[[:space:]]*$parameter" $init_file | awk -F'=' '{gsub(/[[:space:]]*/, "", $2); print $2}')
 
 if [ -z "$command_output" ]; then
     printf "%s = Not found\n" "$parameter_name"
