88bfcc258f7eb4686907d5e09b7692b51882fafd
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-10 18:27:57 +0530

    Update sqlnet_inbound_timeout.sh

--- collectors/sqlnet_inbound_timeout.sh
+++ collectors/sqlnet_inbound_timeout.sh
@@ -6,8 +6,8 @@ sql_file="/sapmnt/${SID}/profile/oracle/sqlnet.ora"
 # Extract the actual value (ignoring case)
 ACTUAL_TIMEOUT=$(grep -i "^$parameter" "$sql_file" 2>/dev/null | awk -F '=' '{print $2}' | tr -d ' ')
 
-if [ -z "$command_output" ]; then
+if [ ! -f "$sql_file" ] || [ -z "$ACTUAL_TIMEOUT" ]; then
     printf "%s = Not found\n" "$parameter_name"
 else
-    printf "%s = %s\n" "$parameter_name" "$command_output"
+    printf "%s = %s\n" "$parameter_name" "$ACTUAL_TIMEOUT"
 fi
