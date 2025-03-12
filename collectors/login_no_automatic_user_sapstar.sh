c69b32d8abaacde10ff627115218dec60d800f1b
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-12 15:34:15 +0530

    Add files via upload

---
+++ collectors/login_no_automatic_user_sapstar.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Set Variables
+SID="$SAP_SID"
+inst_no="$app_inst_no"
+host="$app_host"
+param_to_check="login/no_automatic_user_sapstar"
+#expected_value="1"
+
+# Define profile paths in order of priority
+profiles=(
+    "/sapmnt/${SID}/profile/DEFAULT.PFL"
+    "/sapmnt/${SID}/profile/${SID}_DVEBMGS${inst_no}_${host}"
+    "/sapmnt/${SID}/profile/${SID}_D${inst_no}_${host}"
+)
+
+found_value=""  # Variable to store the found value
+
+# Check profiles one by one
+for profile in "${profiles[@]}"; do
+    if [[ -f "$profile" ]]; then
+        parameter_value=$(grep -E "^${param_to_check}[[:space:]]*=" "$profile" | awk -F= '{print $2}' | tr -d ' ')
+
+        if [[ -n "$parameter_value" ]]; then
+            found_value="$parameter_value"
+            break  # Stop checking further profiles
+        fi
+    fi
+done
+
+# Print the final result (only one output)
+if [[ -n "$found_value" ]]; then
+        #if [[ "$found_value" == "$expected_value" ]]; then
+                echo "$param_to_check = $found_value"
+        #else
+                #echo "$param_to_check = $found_value (expected value = $expected_value)"
+        #fi
+else
+    echo "$param_to_check = Not found"
+fi
