0d7f3f4cff719d21997bb79b88ed2d43242da5ef
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-26 17:07:24 +0530

    Update sqlnet_outbound_timeout.sh

--- collectors/sqlnet_outbound_timeout.sh
+++ collectors/sqlnet_outbound_timeout.sh
@@ -4,7 +4,7 @@ parameter_name="SQLNET.OUTBOUND_CONNECT_TIMEOUT"
 sql_file="/sapmnt/${SID}/profile/oracle/sqlnet.ora"
 
 # Extract the actual value (ignoring case)
-ACTUAL_TIMEOUT=$(grep -i "^$parameter" "$sql_file" 2>/dev/null | awk -F '=' '{print $2}' | tr -d ' ')
+ACTUAL_TIMEOUT=$(grep -i "^$parameter_name" "$sql_file" 2>/dev/null | awk -F '=' '{print $2}' | tr -d ' ')
 
 if [ ! -f "$sql_file" ] || [ -z "$ACTUAL_TIMEOUT" ]; then
     printf "%s = Not found\n" "$parameter_name"
