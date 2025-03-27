835968a00efe4d51857af5bf68b4bc8677de3dc9
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-27 13:46:47 +0530

    Update hostagent_kernel_release.sh

--- collectors/hostagent_kernel_release.sh
+++ collectors/hostagent_kernel_release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-USER="${SAP_SID,,}adm"
-
 hostagent_kernel_release=$(/usr/sap/hostctrl/exe/saphostctrl -function ExecuteOperation -name versioninfo 2>/dev/null |  grep -i "kernel release" | awk '{print $NF}')
 
 if [[ -z "$hostagent_kernel_release" ]]; then
