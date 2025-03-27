8e84a628b9c75b131c16f8759e4196e63a7b9d35
SATHSIHK <sathish.k@basiscloudsolutions.com>
2025-03-27 13:46:36 +0530

    Update hostagent_kernel_patch.sh

--- collectors/hostagent_kernel_patch.sh
+++ collectors/hostagent_kernel_patch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-USER="${SAP_SID,,}adm"
-
 hostagent_kernel_patch=$(/usr/sap/hostctrl/exe/saphostctrl -function ExecuteOperation -name versioninfo 2>/dev/null |  grep -i "patch number" | awk '{print $NF}')
 
 if [[ -z "$hostagent_kernel_patch" ]]; then
